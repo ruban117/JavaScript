ff0ca6df9431b591dcf7175d9463bb9d4eb53041
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-03 23:40:50 +0530

    Add files via upload

---
+++ 03_Primitive_DataType_JS.js
@@ -0,0 +1,25 @@
+//There are 7 types of primitive data types in Javascript
+
+let a=null  //Null
+let b=200   //Number
+let c="Ruban Pathak"    //String
+let d=Symbol("This Is A Symbol")    //Symbol
+let e=undefined //Undefined
+let f=true  //Boolean
+let g=BigInt(100)   //Bigint
+
+console.log(a,b,c,d,e,f,g);
+
+
+//Object
+
+let o={
+    "Ruban":117,
+    "Soumita":96,
+    "Altab":88,
+    "Shobhan":85,
+    "Souvik":108
+}
+
+console.log(o)
+console.log(o["Ruban"]) //Accessing Value from a specific object

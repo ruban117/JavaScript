03e8bb9579c796c86560e5f56129fc3711467adf
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-04 10:06:42 +0530

    Add files via upload

---
+++ 05_Expressions_Conditions_Operators_IN_JS.js
@@ -0,0 +1,61 @@
+//Expressions in JS
+
+77;
+"Ruban Pathak";
+true;
+
+
+//Operators In JS
+
+/*i) Arithmetic Operators*/
+
+let a=10
+let b=20
+console.log("a + b =",a+b)
+console.log("a - b =",a-b)
+console.log("a / b =",a/b)
+console.log("a * b =",a*b)
+console.log("a ** b =",a**b)
+console.log("a % b =",a%b)
+console.log("a ++ =",a++)
+console.log("a -- =",a--)
+console.log("++a =",++a)
+console.log("--a =",--a)
+
+/*ii) Assignment Operators*/
+
+let c=5
+
+c+=5
+console.log("c += 5 =",c)
+c-=5
+console.log("c -= 5 =",c)
+c*=5
+console.log("c *= 5 =",c)
+c/=5
+console.log("c /= 5 =",c)
+c%=5
+console.log("c %= 5 =",c)
+c**=5
+console.log("c **= 5 =",c)
+
+/*Comparison Operator*/
+
+let d=27,e="27"
+
+console.log("d == e =",d==e)
+console.log("d != e =",d!=e)
+console.log("d === e =",d===e)
+console.log("d !== e =",d!==e)
+console.log("d > e =",d>e)
+console.log("d < e =",d<e)
+console.log("d >= e =",d>=e)
+console.log("d <= e =",d<=e)
+
+/*Logical Operator*/
+
+let f=44,g=45
+
+console.log("f && g =",(f>g)&&(f<g))
+console.log("f || g =",(f>g)||(f<g))
+console.log("f ! g =",!f)

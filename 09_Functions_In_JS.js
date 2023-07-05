64818088a4f96ebd02fc349319a16dd8e872b053
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-05 10:22:57 +0530

    Add files via upload

---
+++ 09_Functions_In_JS.js
@@ -0,0 +1,16 @@
+/*Function declaration old type*/
+
+function sum(a,b){
+    return a+b
+}
+
+console.log(sum(12,50))
+
+
+/*Arrow Function*/
+
+const sum2=(a,b)=>{
+    return a+b
+}
+
+console.log(sum2(10,20))

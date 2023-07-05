caaff2c8e36a66be46dc59d72de643da214cba0c
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-06 00:27:15 +0530

    Add files via upload

---
+++ 11_Strings_In_JS.js
@@ -0,0 +1,30 @@
+/*Creating Strings*/
+
+//Double quotted string
+
+let n="Ruban"
+console.log(n.length)//length of the string 5
+
+//Single quotted string
+
+let n2='Ruban'
+console.log(n2)
+
+//Template Literals
+
+let n3=`Ruban "Pathak"`
+console.log(n3)
+
+//String Interpolation
+
+n4="Ruban"
+n5="Soumita"
+
+console.log(`${n4} is a good friend of ${n5}`)
+
+//Escape Sequence
+
+n6='Adam D \'Angelo'
+console.log(n6)
+
+console.log("Jurassic\nPark\nPUBG\tMOBILE\nCOD\rMOBILE")//use of escape sequence

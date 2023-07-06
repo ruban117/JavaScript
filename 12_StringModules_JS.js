935071bf07f5c72730ebfa3dcfbddad4557014b3
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-06 19:57:30 +0530

    Add files via upload

---
+++ 12_StringModules_JS.js
@@ -0,0 +1,43 @@
+/*Here are the examples of some string modules*/
+
+/*length*/
+
+var n="Ruban"
+console.log(n.length)
+
+/*Uppercase*/
+
+var n="Ruban"
+console.log(n.toUpperCase())
+
+/*Lowercase*/
+
+var n="Ruban"
+console.log(n.toLowerCase())
+
+/*slice*/
+
+var n="Ruban"
+console.log(n.slice(2,4))
+
+var n="Ruban"
+console.log(n.slice(2))
+
+/*Replace*/
+
+var n="Ruban Bhai"
+console.log(n.replace("Bhai","Bhau"))
+
+/*Concatinate*/
+
+var n="Ruban"
+console.log(n.concat(" ","Soumita"))
+
+var n="Ruban"
+console.log(n+" "+"Soumita")
+
+/*remove whitespace*/
+
+var n="            Ruban         "
+console.log(n.trim())
+

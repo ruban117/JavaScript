7a35d40b4dd4dd6da390dace87bc6b6631775de9
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-08 21:17:55 +0530

    Add files via upload

---
+++ 22_Interaction_Alert_Prompt_Confirm_JS/script.js
@@ -0,0 +1,22 @@
+/*Here are some importent interaction methods in js*/
+
+
+alert("This Is An gental alert")//alert method
+
+/*prompt method*/
+
+let a=prompt("Enter Any Number:- ")
+
+
+
+
+/*Confirm method*/
+
+let write=confirm("Do you want to add it into the webpage?:- ")
+
+if(write){
+    document.write(a)
+}
+else{
+    document.write("You have to press yes!")
+}

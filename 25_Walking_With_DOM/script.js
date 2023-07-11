07cc991ffeeb4abba141c0c47e1817b17e18672d
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-11 10:04:44 +0530

    Add files via upload

---
+++ 25_Walking_With_DOM/script.js
@@ -0,0 +1,11 @@
+document.body //gets body tag
+
+document.title //gets the text of the title tag
+
+
+document.documentElement //gets all the tags of your webpage
+
+document.head  //gets the head tag
+
+
+/*you have to write all of these in your browser console*/

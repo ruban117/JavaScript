6e37ff0d85794f9fcc4f7dc2f722d26698b97d91
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-11 10:33:08 +0530

    Add files via upload

---
+++ 26_Children_Of_An_Element_JS/script.js
@@ -0,0 +1,7 @@
+console.log(Array.from(document.body.childNodes))
+
+console.log(document.body.firstChild)
+
+console.log(document.body.lastChild)
+
+console.log(document.body.childNodes[document.body.childNodes.length-1])

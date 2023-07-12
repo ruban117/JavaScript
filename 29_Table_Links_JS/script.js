5a81f7d87b576e121d2306baef1dc35222476b49
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-12 19:43:28 +0530

    Add files via upload

---
+++ 29_Table_Links_JS/script.js
@@ -0,0 +1,15 @@
+let t=document.body.firstElementChild
+
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.rows[0].rowIndex)
+
+
+
+/*Quick Quiz*/
+
+console.log(typeof document)
+console.log(typeof window)

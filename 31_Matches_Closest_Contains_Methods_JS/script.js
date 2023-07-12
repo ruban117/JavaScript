a0bbe1259c3cb12dc7ff0417fe1d374f3e0043aa
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-12 22:46:15 +0530

    Add files via upload

---
+++ 31_Matches_Closest_Contains_Methods_JS/script.js
@@ -0,0 +1,8 @@
+let id1=document.getElementById("id1")
+let sp1=document.getElementById("sp1")
+console.log(id1.matches(".c1"))
+console.log(sp1.matches(".sc1"))
+
+console.log(sp1.closest(".c1"))
+
+console.log(id1.contains(sp1))

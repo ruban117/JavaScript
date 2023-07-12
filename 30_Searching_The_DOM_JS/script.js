f773ff866dcd2cf390fe062f2be73bd2ee017854
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-12 21:36:42 +0530

    Add files via upload

---
+++ 30_Searching_The_DOM_JS/script.js
@@ -0,0 +1,10 @@
+let c=document.getElementById("tt")
+c.style.color="red"
+
+console.log(document.querySelectorAll(".card-title"))
+console.log(document.querySelectorAll(".card-title")[0])
+
+console.log(document.body.firstElementChild.getElementsByTagName("a"))
+
+document.querySelector(".card-title").style.color="red"
+

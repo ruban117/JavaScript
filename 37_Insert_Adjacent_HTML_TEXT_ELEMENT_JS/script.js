7294fea8166db0898907b26e125d7a1d5dd3520f
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-14 19:37:39 +0530

    Add files via upload

---
+++ 37_Insert_Adjacent_HTML_TEXT_ELEMENT_JS/script.js
@@ -0,0 +1,5 @@
+let a=document.getElementById('id1')
+a.insertAdjacentHTML('beforeend','<p>BEFOREEND</p>')
+a.insertAdjacentHTML('beforebegin','<p>BEFOREBEGIN</p>')
+a.insertAdjacentHTML('afterend','<p>AFTEREND</p>')
+a.insertAdjacentHTML('afterbegin','<p>AFTERBEGIN</p>')

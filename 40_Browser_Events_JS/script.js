8ad1d418fa171c735c3e352837573177b4d09952
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-15 00:00:35 +0530

    Add files via upload

---
+++ 40_Browser_Events_JS/script.js
@@ -0,0 +1,13 @@
+let a=document.getElementsByClassName('hola')[0]
+
+a.onclick=()=>{
+    let b=document.getElementsByClassName('hola')[0]
+    b.innerHTML="<h1>Rubu</h1>"
+}
+
+
+let c=document.getElementById('btn')
+
+c.onclick=()=>{
+    alert("Hello World")
+}

0558297af4e0db5862bd5e52eedc46d5d396afd5
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-14 20:13:52 +0530

    Add files via upload

---
+++ 38_ClassName_And_ClassList/script.js
@@ -0,0 +1,11 @@
+
+
+let a=document.getElementById('id1')
+
+console.log(a.classList)
+//a.classList.remove("red")
+a.classList.add("yellow")
+
+//console.log(a.classList.toggle("yellow"))
+console.log(a.classList.contains("yellow"))
+console.log(a.classList.contains("red"))

0b6a265b9d9eb4fe5014343428279f2c9f74b616
Ruban Pathak <102974324+ruban117@users.noreply.github.com>
2023-07-20 10:44:26 +0530

    Add files via upload

---
+++ 51_Try_Catch_Block_JS/script.js
@@ -0,0 +1,24 @@
+let a=prompt("Enter The Number:- ")
+let b=prompt("Enter The Divident Number:- ")
+try{
+    console.log(a/b)
+    dsds
+}
+catch(error){
+    console.log("You have generated some error the error is:- "+error)
+}
+
+
+/*you cannot handle the error like this */
+/*try{
+    setTimeout(()=>{
+        //console.log(a/b)
+        dsds
+    },5000)
+}
+catch(error){
+    console.log("You have generated some error the error is:- "+error)
+}*/
+
+
+
